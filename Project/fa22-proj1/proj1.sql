7107f5ccf385dc0f64a086b9569ad797e66fbb70
Evan Day <yichaoday_evan@berkeley.edu>
2023-01-11 01:06:54 -0800

    proj-1

--- Project/fa22-proj1/proj1.sql
+++ Project/fa22-proj1/proj1.sql
@@ -98,26 +98,62 @@ AS
 -- Question 3i
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, p.namefirst, p.namelast, a.yearid, round(d1 * 1.0 / d2, 4) slg
+  from people p, (
+	SELECT playerid, yearid, SUM(H) + 1 * SUM(H2B) + 2 * SUM(H3B) + 3 * SUM(HR) d1, SUM(AB) d2
+    FROM batting
+    GROUP BY playerid, yearid, teamID
+    HAVING SUM(AB) > 50
+    ) a
+  WHERE p.playerid = a.playerid and slg > 0
+  ORDER BY slg DESC, a.yearid, p.playerid
+  limit 10
 ;
 
 -- Question 3ii
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, p.namefirst, p.namelast, round(d1 * 1.0 / d2, 4) lslg
+  from people p, (
+	SELECT playerid, SUM(H) + 1 * SUM(H2B) + 2 * SUM(H3B) + 3 * SUM(HR) d1, SUM(AB) d2
+    FROM batting
+    GROUP BY playerid
+    HAVING SUM(AB) > 50
+    ) a
+  WHERE p.playerid = a.playerid and lslg > 0
+  ORDER BY lslg DESC, p.playerid
+  limit 10
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT p.namefirst, p.namelast, round(a.d1 * 1.0 / a.d2, 4) lslg
+  from people p, (
+	SELECT playerid, SUM(H) + 1 * SUM(H2B) + 2 * SUM(H3B) + 3 * SUM(HR) d1, SUM(AB) d2
+    FROM batting
+    GROUP BY playerid
+    HAVING SUM(AB) > 50
+    ) a, 
+    (
+	SELECT SUM(H) + 1 * SUM(H2B) + 2 * SUM(H3B) + 3 * SUM(HR) d1, SUM(AB) d2
+    FROM batting
+    WHERE playerid = "mayswi01"
+    ) b 
+    
+  WHERE p.playerid = a.playerid and lslg > 0 and lslg > round(b.d1 * 1.0 / b.d2, 4)
+  ORDER BY lslg DESC, p.playerid
 ;
 
 -- Question 4i
 CREATE VIEW q4i(yearid, min, max, avg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT yearid, min(salary), max(salary), avg(salary)
+  from salaries
+  group by yearid
+  order by yearid
 ;
+
 
 -- Question 4ii
 CREATE VIEW q4ii(binid, low, high, count)
@@ -128,16 +164,54 @@ AS
 -- Question 4iii
 CREATE VIEW q4iii(yearid, mindiff, maxdiff, avgdiff)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  -- get the stats that I want: table t
+  WITH
+  t AS (
+    SELECT s.yearid, MIN(s.salary) min, MAX(s.salary) max, AVG(s.salary) avg
+    FROM salaries AS s
+    GROUP BY s.yearid
+  ),  -- don't forget the comma
+
+  -- t1.yearsid = yearsid + 1
+  t1 AS (
+      SELECT s.yearid + 1 as yearid, MIN(s.salary) min, MAX(s.salary) max, AVG(s.salary) avg
+      FROM salaries AS s
+      GROUP BY yearid
+     )
+
+     -- join t, t1 and I have #s for both yearid and yearid + 1 to work with
+     SELECT t.yearid, t.min - t1.min AS mindiff,
+                      t.max - t1.max AS maxdiff,
+                      t.avg - t1.avg AS avgdiff
+     FROM t1 INNER JOIN t ON t1.yearid = t.yearid
+     ORDER BY t1.yearid ASC
 ;
 
 -- Question 4iv
 CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
-;
+	WITH t AS (
+	  SELECT s.yearid, MAX(s.salary) max
+	  FROM salaries AS s
+	  WHERE s.yearid = 2000 OR s.yearid = 2001
+	  GROUP BY s.yearid
+	)
+
+	SELECT s.playerid, p.namefirst, p.namelast, s.salary, s.yearid
+	FROM people AS p
+		  INNER JOIN salaries AS s
+			ON p.playerid = s.playerid
+		  INNER JOIN t
+			ON s.salary = t.max AND s.yearid = t.yearid
+	;
+
 -- Question 4v
 CREATE VIEW q4v(team, diffAvg) AS
-  SELECT 1, 1 -- replace this line
-;
+	SELECT a.teamid as team, MAX(s.salary) - MIN(s.salary) AS diffAvg
+	  FROM allstarfull AS a
+	  JOIN salaries AS s on s.playerid = a.playerid
+	  WHERE a.yearid = 2016 and s.yearid = 2016
+	  GROUP BY a.teamid
+	  ORDER BY a.teamid
+	;
 

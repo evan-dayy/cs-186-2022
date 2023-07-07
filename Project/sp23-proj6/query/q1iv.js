5d441d12eade3b676e13a263079f678edd0eb4f3
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 10:13:12 -0700

    q1-iv

--- Project/sp23-proj6/query/q1iv.js
+++ Project/sp23-proj6/query/q1iv.js
@@ -1,5 +1,18 @@
 // Task 1iv
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.ratings.aggregate([
+    {$match: {userId: 186}},
+    {$sort: {timestamp: -1}},
+    {$limit: 5},
+    {$group: 
+        {
+            _id: null,
+            movieIds : {$push: "$movieId"},
+            ratings : {$push : "$rating"},
+            timestamps : {$push : "$timestamp"}
+        }},
+        {$project: {
+            _id: 0
+        }}
+    
 ]);

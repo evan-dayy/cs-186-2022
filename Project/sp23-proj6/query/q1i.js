cc5ecb8d44b10b6b29c46a1e0120bc428e8354e1
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 09:39:43 -0700

    q1

--- Project/sp23-proj6/query/q1i.js
+++ Project/sp23-proj6/query/q1i.js
@@ -1,5 +1,24 @@
 // Task 1i
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.keywords.aggregate([
+    {
+        $match: {$or : [{keywords: {$elemMatch: {name : "mickey mouse"}}}, 
+                     {keywords: {$elemMatch: {name : "marvel comic"}}}]}
+    },
+    {
+        $lookup: {
+            from: "movies_metadata",
+            localField: "movieId",
+            foreignField: "movieId",
+            as : "movies"
+        }
+    },
+    {$sort: {movieId: 1}},
+    {
+        $project: {
+            _id: 0,
+            movieId : 1
+        }
+    }
+    
 ]);

1570721af513fd7d0c97afa0ad3dbf64d6215540
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 10:27:12 -0700

    q2-i

--- Project/sp23-proj6/query/q2i.js
+++ Project/sp23-proj6/query/q2i.js
@@ -1,5 +1,19 @@
 // Task 2i
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.movies_metadata.aggregate([
+    {$project:
+        {
+            _id: 0,
+            title: 1,
+            vote_count: 1,
+            score : {
+                $add: [
+                    {$multiply: ["$vote_average", {$round : {$divide: ["$vote_count", {$add: ["$vote_count", 1838]}]}}]},
+                    {$multiply: [7, {$round : {$divide: [1838, {$add: ["$vote_count", 1838]}]}}]},
+                ]
+            }
+        }
+    },
+    {$sort: {score: -1}},
+    {$limit: 20}
 ]);

79a2dc41ad71ea318cd0d885ef477b9959002288
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 12:18:25 -0700

    q3-ii

--- Project/sp23-proj6/query/q3ii.js
+++ Project/sp23-proj6/query/q3ii.js
@@ -1,5 +1,23 @@
 // Task 3ii
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.credits.aggregate([
+    {$match: 
+        {crew: 
+            {$elemMatch: {
+                id : {$eq: 5655},
+                job : {$eq: "Director"}
+            }
+        }}
+    },
+    {$project: {_id: 0, cast: 1}},
+    {$unwind: "$cast"},
+    {$group: {_id: {val1: "$cast.id", val2: "$cast.name"}, count: {$sum: 1}}},
+    {$project: {
+        _id: 0,
+        count: 1,
+        id: "$_id.val1",
+        name: "$_id.val2"
+    }},
+    {$sort: {count: -1, id: 1}},
+    {$limit: 5}
 ]);

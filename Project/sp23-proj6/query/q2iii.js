f956c3399db79c3112104888d1ff83ca5dda7245
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 11:33:39 -0700

    q2-iii

--- Project/sp23-proj6/query/q2iii.js
+++ Project/sp23-proj6/query/q2iii.js
@@ -1,5 +1,17 @@
 // Task 2iii
-
-db.todo.aggregate([
-    // TODO: Write your query here
+db.movies_metadata.aggregate([
+    {$project : {_id: 0, budget: 1}},
+    {$match: {$and: [{budget : {$ne: false}}, 
+                    {budget :{$ne: 0}}, 
+                    {budget :{$ne: null}},
+                    {budget :{$ne: ''}}]}},
+    {$project: {budget: 
+        {$cond: {if: {$isNumber: '$budget'}, 
+                then: '$budget', 
+                else: {$toInt: {$trim: {input: '$budget', chars: 'USD\$ '}}}}}}
+            },
+    {$project: {budget: {$round: ['$budget', -7]}}},
+    {$group: {_id: "$budget", count: {$sum: 1}}},
+    {$project: {_id: 0, budget: "$_id", count: 1}},
+    {$sort: {budget: 1}}
 ]);

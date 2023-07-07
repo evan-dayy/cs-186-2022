3d96954a490aedf9c7b189d4a551bec0f387d1b1
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 10:05:24 -0700

    q1-iii

--- Project/sp23-proj6/query/q1iii.js
+++ Project/sp23-proj6/query/q1iii.js
@@ -1,5 +1,10 @@
 // Task 1iii
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.ratings.aggregate([
+    {$group:
+        {_id: "$rating",
+        count: {$sum: 1}
+    }},
+    {$sort: {_id: -1}},
+    {$project: {_id: 0, count: 1, rating: "$_id"}}
 ]);

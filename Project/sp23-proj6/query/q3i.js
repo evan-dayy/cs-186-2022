f24ac05eeb77cb2a78aec0dadcef504a468d2fcf
Evan Day <yichaoday_evan@berkeley.edu>
2023-07-07 11:51:15 -0700

    q3-i

--- Project/sp23-proj6/query/q3i.js
+++ Project/sp23-proj6/query/q3i.js
@@ -1,5 +1,20 @@
 // Task 3i
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.credits.aggregate([
+    {$match: {"crew.id" : {$eq: 7624}}},
+    {$project: {_id: 0, movieId: 1, cast: 1}},
+    {$unwind: "$cast"},
+    {$match: {"cast.id" : {$eq: 7624}}},
+    {$project: {movieId: 1, character: "$cast.character"}},
+    {$lookup: {
+        from: "movies_metadata",
+        localField: "movieId",
+        foreignField: "movieId",
+        as: "movie"
+    }},
+    {$sort: {"movie.release_date": -1}},
+    {$project: {title: {$first : "$movie.title"}, 
+                release_date: {$first : "$movie.release_date"}, 
+                character: 1}}
+    
 ]);
